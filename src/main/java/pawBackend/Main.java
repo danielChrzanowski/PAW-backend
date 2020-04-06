d04e127d24c6161c2cd6cad5f90d7b6509560079
Daniel <daniel_chrzanowski98@wp.pl>
2020-04-06 14:40:32 +0200

    renamed

--- src/main/java/pawBackend/Main.java
+++ src/main/java/pawBackend/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PznuJavaApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(PznuJavaApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
 }
--- src/test/java/pawBackend/PznuJavaApplicationTest.java
+++
@@ -1,5 +0,0 @@
-package pawBackend;
-
-class PznuJavaApplicationTest {
-
-}
